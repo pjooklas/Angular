03f2fec0acb34f4019920d35d34a585ab1f7ccf3
pjooklas <pjooklas@users.noreply.github.com>
2022-05-23 19:41:10 +0300

    typescript variables

--- typescript_pagrindai/main.js
+++ typescript_pagrindai/main.js
@@ -1,6 +1,30 @@
-function isvestiKainaSuAntkainiu(kaina, antkainis) {
-    console.log("Kainuos " + (kaina + antkainis) + " EUR");
-}
 var kaina = 5;
 var antkainis = 2;
 isvestiKainaSuAntkainiu(kaina, antkainis);
+var x;
+var y;
+var z; //null jei neegzistuoja/neivestas
+var t; //wrapper
+z = "Labas";
+t = "Viso";
+//z=t negalima priskirti, nes t yra wrapperis
+var h;
+var p;
+var k;
+var o;
+var Spalvos;
+(function (Spalvos) {
+    Spalvos[Spalvos["Red"] = 0] = "Red";
+    Spalvos[Spalvos["Green"] = 1] = "Green";
+    Spalvos[Spalvos["Blue"] = 2] = "Blue";
+})(Spalvos || (Spalvos = {}));
+;
+var u;
+u = Spalvos.Blue;
+function isvestiKainaSuAntkainiu(kaina, antkainis) {
+    console.log("Kainuos " + (kaina + antkainis) + " EUR");
+}
+var isv = function (kaina, antkainis) {
+    console.log("Kainuos " + (kaina + antkainis) + " EUR");
+};
+isv(8, 9);
--- typescript_pagrindai/main.ts
+++ typescript_pagrindai/main.ts
@@ -1,9 +1,38 @@
-function isvestiKainaSuAntkainiu (kaina:number, antkainis:number){
-    console.log("Kainuos " + (kaina + antkainis)+ " EUR");    
-}
+
 
 let kaina = 5;
 let antkainis = 2;
 
 isvestiKainaSuAntkainiu(kaina, antkainis);
 
+let x:number;
+let y:boolean;
+let z:string|null; //null jei neegzistuoja/neivestas
+
+let t:String; //wrapper
+z="Labas";
+t="Viso";
+//z=t negalima priskirti, nes t yra wrapperis
+
+let h:number[];
+let p:string[];
+let k:any[];
+let o:number|string[];
+
+enum Spalvos {
+    Red,
+    Green,
+    Blue
+};
+let u:Spalvos;
+u = Spalvos.Blue;
+ 
+function isvestiKainaSuAntkainiu (kaina:number, antkainis:number):void{  //void reiskia bet ka, buna po default'u
+    console.log("Kainuos " + (kaina + antkainis)+ " EUR");    
+}
+
+let isv= (kaina:number, antkainis:number) =>{
+    console.log("Kainuos " + (kaina + antkainis)+ " EUR");    
+}
+
+isv(8,9);
