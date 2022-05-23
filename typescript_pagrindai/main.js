89da31a0ac80f88fe4339d728f6a962d592c2646
pjooklas <pjooklas@users.noreply.github.com>
2022-05-23 18:47:43 +0300

    typescript basics

---
+++ typescript_pagrindai/main.js
@@ -0,0 +1,6 @@
+function isvestiKainaSuAntkainiu(kaina, antkainis) {
+    console.log("Kainuos " + (kaina + antkainis) + " EUR");
+}
+var kaina = 5;
+var antkainis = 2;
+isvestiKainaSuAntkainiu(kaina, antkainis);
---
+++ typescript_pagrindai/main.ts
@@ -0,0 +1,9 @@
+function isvestiKainaSuAntkainiu (kaina:number, antkainis:number){
+    console.log("Kainuos " + (kaina + antkainis)+ " EUR");    
+}
+
+let kaina = 5;
+let antkainis = 2;
+
+isvestiKainaSuAntkainiu(kaina, antkainis);
+
