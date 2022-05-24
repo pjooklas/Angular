a0d435baa327f2bf4b12300d099d85782f526227
pjooklas <pjooklas@users.noreply.github.com>
2022-05-24 19:26:44 +0300

    KMI skaiciuokle + bootstrap

--- typescript_kmi/app.js
+++ typescript_kmi/app.js
@@ -1,1 +1,31 @@
-console.log("labas");
+"use strict";
+/*
+const output=document.getElementById("output");
+const btnOutput=document.getElementById("btnOutput");
+const inputText=<HTMLInputElement|null>document.getElementById("inText");
+
+const isvedimas=()=>{
+    if(output!=null && inputText!=null) {
+       output.innerHTML=(inputText.valueAsNumber**2).toString();
+    }
+}
+
+if (btnOutput!=null) {
+    btnOutput.onclick= isvedimas;
+}
+*/
+const svoris = document.getElementById("inSvoris");
+const ugis = document.getElementById("inUgis");
+const btnKMI = document.getElementById("btnKMI");
+const outKMI = document.getElementById("kmi");
+const skaiciuotiKMI = () => {
+    if (svoris != null && ugis != null && outKMI != null) {
+        let svorisVal = svoris.valueAsNumber;
+        let ugisVal = ugis.valueAsNumber / 100;
+        let kmi = svorisVal / (ugisVal ** 2);
+        outKMI.innerHTML = kmi.toFixed(2);
+    }
+};
+if (btnKMI != null) {
+    btnKMI.onclick = skaiciuotiKMI;
+}
--- typescript_kmi/app.ts
+++ typescript_kmi/app.ts
@@ -1,1 +1,33 @@
-console.log("labas");
+/*
+const output=document.getElementById("output");
+const btnOutput=document.getElementById("btnOutput");
+const inputText=<HTMLInputElement|null>document.getElementById("inText");
+
+const isvedimas=()=>{
+    if(output!=null && inputText!=null) {
+       output.innerHTML=(inputText.valueAsNumber**2).toString();
+    }
+}
+
+if (btnOutput!=null) {
+    btnOutput.onclick= isvedimas;
+}
+*/
+const svoris=<HTMLInputElement|null>document.getElementById("inSvoris");
+const ugis=<HTMLInputElement|null>document.getElementById("inUgis");
+const btnKMI=document.getElementById("btnKMI");
+const outKMI=document.getElementById("kmi");
+
+
+const skaiciuotiKMI = () => {
+    if (svoris!=null && ugis!=null && outKMI!=null) {
+        let svorisVal=svoris.valueAsNumber;
+        let ugisVal=ugis.valueAsNumber/100;
+        let kmi=svorisVal/(ugisVal**2);
+        outKMI.innerHTML=kmi.toFixed(2);
+    }
+}
+
+if (btnKMI!=null) {
+    btnKMI.onclick=skaiciuotiKMI;
+}
