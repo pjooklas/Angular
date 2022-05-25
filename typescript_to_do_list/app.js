1f5caee809e9e5011de53aadb7d743e8ef20a546
pjooklas <pjooklas@users.noreply.github.com>
2022-05-25 20:49:43 +0300

    to do app delete fnction

--- typescript_to_do_list/app.js
+++ typescript_to_do_list/app.js
@@ -2,17 +2,35 @@
 const taskName = document.getElementById("taskName");
 const btnAddTask = document.getElementById("btnAddTask");
 const taskList = document.getElementById("taskList");
+const btnDeleteTasks = document.getElementById("deleteTasks");
 let tasks = [];
 const showTasks = () => {
     if (taskList != null) {
         taskList.innerHTML = "";
-        tasks.forEach((task) => {
+        tasks.forEach((task, i) => {
             const li = document.createElement("li");
             li.className = "list-group-item";
             li.textContent = task;
+            const btn = document.createElement("button");
+            btn.textContent = "Atlikta";
+            btn.className = "btn btn-info float-end";
+            btn.onclick = () => {
+                deleteTask(i);
+            };
+            li.appendChild(btn);
             taskList.appendChild(li);
         });
     }
+};
+const deleteTasks = () => {
+    localStorage.removeItem("toDoList");
+    tasks = [];
+    showTasks();
+};
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    saveTasks();
+    showTasks();
 };
 let jsonString = localStorage.getItem("toDoList");
 if (jsonString != null) {
@@ -24,10 +42,16 @@ const addTask = () => {
         tasks.push(taskName.value);
         taskName.value = "";
         console.log(tasks);
-        localStorage.setItem("toDoList", JSON.stringify(tasks));
+        saveTasks();
         showTasks();
     }
 };
+const saveTasks = () => {
+    localStorage.setItem("toDoList", JSON.stringify(tasks));
+};
 if (btnAddTask != null) {
     btnAddTask.onclick = addTask;
+}
+if (btnDeleteTasks != null) {
+    btnDeleteTasks.onclick = deleteTasks;
 }
--- typescript_to_do_list/app.ts
+++ typescript_to_do_list/app.ts
@@ -1,6 +1,7 @@
 const taskName = <HTMLInputElement|null>document.getElementById("taskName");
 const btnAddTask = document.getElementById("btnAddTask");
 const taskList = document.getElementById("taskList");
+const btnDeleteTasks = document.getElementById("deleteTasks");
 
 let tasks:string[]=[];
 
@@ -8,14 +9,34 @@ const showTasks=()=>{
     if (taskList!=null) {
         taskList.innerHTML="";
         
-        tasks.forEach((task)=>{
+        tasks.forEach((task, i)=>{
             const li=document.createElement("li");
             li.className="list-group-item";
             li.textContent=task;
+            const btn=document.createElement("button");
+            btn.textContent="Atlikta";
+            btn.className="btn btn-info float-end";
+            btn.onclick=()=>{
+                deleteTask(i);
+            }
+            li.appendChild(btn);
             taskList.appendChild(li);
         });
     }
 }
+
+const deleteTasks=()=>{
+    localStorage.removeItem("toDoList");
+    tasks=[];
+    showTasks();
+}
+
+const deleteTask=(index:number)=>{
+    tasks.splice(index, 1);
+    saveTasks();
+    showTasks();
+};
+
 
 let jsonString= localStorage.getItem("toDoList");
 if (jsonString!=null) {
@@ -28,14 +49,19 @@ const addTask=()=>{
         tasks.push(taskName.value);
         taskName.value="";
         console.log(tasks);
-
-        localStorage.setItem("toDoList", JSON.stringify(tasks));
-
+        saveTasks();
         showTasks();
-
     }
+};
+
+const saveTasks=()=>{
+    localStorage.setItem("toDoList", JSON.stringify(tasks));
 };
 
 if (btnAddTask!=null) {
     btnAddTask.onclick=addTask;
+}
+
+if (btnDeleteTasks!=null) {
+    btnDeleteTasks.onclick=deleteTasks;
 }
