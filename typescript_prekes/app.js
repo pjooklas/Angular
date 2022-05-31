3d8618e26e710a1912f3a532ecb7c5def9499776
pjooklas <pjooklas@users.noreply.github.com>
2022-05-31 21:22:21 +0300

    TS prekes prideti

--- typescript_prekes/app.js
+++ typescript_prekes/app.js
@@ -32,8 +32,12 @@ class Preke {
 }
 const tableBody = document.getElementById("tableBody");
 const sandelioVerte = document.getElementById("sandelioVerte");
+const inputName = document.getElementById("inputName");
+const inputKaina = document.getElementById("inputKaina");
+const inputKiekis = document.getElementById("inputKiekis");
+const buttonPrideti = document.getElementById("buttonPrideti");
 let sandelis = [];
-let sandelioSuma = (sandelis) => {
+const sandelioSuma = (sandelis) => {
     let suma = 0;
     sandelis.forEach((preke) => {
         let prekesSuma = preke.kaina * preke.kiekis;
@@ -42,19 +46,21 @@ let sandelioSuma = (sandelis) => {
     return suma;
 };
 const showSandelis = () => {
+    if (tableBody != null)
+        tableBody.innerHTML = "";
     sandelis.forEach((preke) => {
         const tr = document.createElement("tr");
         let td = document.createElement("td");
         td.textContent = preke.pavadinimas;
         tr.appendChild(td);
         td = document.createElement("td");
-        td.textContent = preke.kaina.toString();
+        td.textContent = preke.kaina + " EUR";
         tr.appendChild(td);
         td = document.createElement("td");
         td.textContent = preke.kiekis.toString();
         tr.appendChild(td);
         td = document.createElement("td");
-        td.textContent = preke.suma.toString();
+        td.textContent = preke.suma + " EUR";
         tr.appendChild(td);
         tableBody === null || tableBody === void 0 ? void 0 : tableBody.appendChild(tr);
     });
@@ -62,9 +68,28 @@ const showSandelis = () => {
         sandelioVerte.textContent = sandelioSuma(sandelis).toString();
     }
 };
-sandelis.push(new Preke("Obuoliai", 1.10, 10));
-sandelis.push(new Preke("Duona", 2, 5));
-sandelis.push(new Preke("Pienas", 1.5, 3));
-console.log(sandelis);
-console.log(sandelioSuma(sandelis));
+const pridetiPreke = () => {
+    if (inputName != null && inputKiekis != null && inputKaina != null) {
+        sandelis.push(new Preke(inputName.value, inputKaina.valueAsNumber, inputKiekis.valueAsNumber));
+        localStorage.setItem("krepselis", JSON.stringify(sandelis));
+        inputName.value = "";
+        inputKaina.value = "";
+        inputKiekis.value = "";
+        showSandelis();
+    }
+};
+const uzkrautiSandeli = () => {
+    let jsonString = localStorage.getItem("krepselis");
+    if (jsonString != null) {
+        let data = JSON.parse(jsonString);
+        data.forEach((d) => {
+            sandelis.push(new Preke(d._pavadinimas, d._kaina, d._kiekis));
+        });
+        console.log(sandelis);
+    }
+};
+uzkrautiSandeli();
 showSandelis();
+if (buttonPrideti != null) {
+    buttonPrideti.onclick = pridetiPreke;
+}
--- typescript_prekes/app.ts
+++ typescript_prekes/app.ts
@@ -27,42 +27,46 @@ class Preke{
         return this._kaina;
     }
 
-    set kaina(value:number){
+    set kaina(value){
         this._kaina=value;
     }
 
     get suma(){
-        return this._kaina * this._kiekis;
+        return this._kaina*this._kiekis;
     }
 
     public kainaSuPVM(){
-        return this._kaina * 1.21;
+        return this._kaina*1.21;
     }
-
 }
-
-const tableBody = document.getElementById("tableBody");
-const sandelioVerte = document.getElementById("sandelioVerte");
+const tableBody=document.getElementById("tableBody");
+const sandelioVerte=document.getElementById("sandelioVerte");
+const inputName=<HTMLInputElement>document.getElementById("inputName");
+const inputKaina=<HTMLInputElement>document.getElementById("inputKaina");
+const inputKiekis=<HTMLInputElement>document.getElementById("inputKiekis");
+const buttonPrideti=document.getElementById("buttonPrideti");
 let sandelis:Preke[]=[];
 
-let sandelioSuma=(sandelis:Preke[])=>{
+const sandelioSuma=(sandelis:Preke[])=>{
     let suma=0;
     sandelis.forEach((preke)=>{
-        let prekesSuma = preke.kaina*preke.kiekis;
+        let prekesSuma=preke.kaina*preke.kiekis;
         suma+=prekesSuma;
     });
     return suma;
 }
 
 const showSandelis=()=>{
+    if (tableBody!=null) tableBody.innerHTML="";
     sandelis.forEach((preke)=>{
         const tr=document.createElement("tr");
+       
         let td=document.createElement("td");
         td.textContent=preke.pavadinimas;
         tr.appendChild(td);
 
         td=document.createElement("td");
-        td.textContent=preke.kaina.toString();
+        td.textContent=preke.kaina+" EUR";
         tr.appendChild(td);
 
         td=document.createElement("td");
@@ -70,25 +74,51 @@ const showSandelis=()=>{
         tr.appendChild(td);
 
         td=document.createElement("td");
-        td.textContent=preke.suma.toString();
+        td.textContent=preke.suma+" EUR";
         tr.appendChild(td);
 
         tableBody?.appendChild(tr);
-    });
+    });  
 
-    if (sandelioVerte!=null) {
+    if (sandelioVerte!=null){
         sandelioVerte.textContent=sandelioSuma(sandelis).toString();
     }
 }
 
+const pridetiPreke=()=>{
+    if (inputName!=null && inputKiekis!=null && inputKaina!=null){
+        sandelis.push(new Preke(inputName.value, inputKaina.valueAsNumber, inputKiekis.valueAsNumber) );
+        localStorage.setItem("krepselis", JSON.stringify(sandelis));
+        inputName.value="";
+        inputKaina.value="";
+        inputKiekis.value="";
+        showSandelis();
+    }
+}
+
+interface dataPreke{
+    _pavadinimas:string,
+    _kaina:number,
+    _kiekis:number
+}
+
+const uzkrautiSandeli=()=>{
+    let jsonString=localStorage.getItem("krepselis");
+    if (jsonString!=null){
+        let data=<dataPreke[]> JSON.parse(jsonString);
+       
+        data.forEach((d)=>{
+            sandelis.push(new Preke(d._pavadinimas,d._kaina,d._kiekis));
+        });
+       
+        console.log(sandelis);
+    }
+}
 
 
-sandelis.push(new Preke("Obuoliai", 1.10, 10));
-sandelis.push(new Preke("Duona", 2, 5));
-sandelis.push(new Preke("Pienas", 1.5, 3));
-console.log(sandelis);
-console.log(sandelioSuma(sandelis));
-
+uzkrautiSandeli();
 showSandelis();
 
-
+if (buttonPrideti!=null){
+    buttonPrideti.onclick=pridetiPreke;
+}
