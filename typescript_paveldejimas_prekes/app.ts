92597bac5f29c5a08adc98d3b276e53e2ad05101
pjooklas <pjooklas@users.noreply.github.com>
2022-06-01 22:02:12 +0300

    TS paveldejimas prekes

---
+++ typescript_paveldejimas_prekes/app.ts
@@ -0,0 +1,74 @@
+class Preke{
+    constructor(
+        private _pavadinimas:string,
+        private _kaina:number,
+        private _kiekis:number
+    ){
+
+    }
+
+    get pavadinimas(){
+        return this._pavadinimas;
+    }
+
+    set pavadinimas(value:string){
+        this._pavadinimas=value;
+    }
+
+    get kiekis(){
+        return this._kiekis;
+    }
+
+    set kiekis(value:number){
+        this._kiekis=value;
+    }
+
+    get kaina(){
+        return this._kaina;
+    }
+
+    set kaina(value){
+        this._kaina=value;
+    }
+
+    get suma(){
+        return this._kaina*this._kiekis;
+    }
+
+    public kainaSuPVM(){
+        return this._kaina*1.21;
+    }
+}
+
+class PrekeKnyga extends Preke{
+    public override kainaSuPVM(): number {
+        return this.kaina*1.09;
+    }
+}
+
+class PrekeVaistai extends Preke{
+    public override kainaSuPVM(): number {
+        return this.kaina*1.05;
+    }
+}
+
+
+let krepselis:Preke[]=[];
+
+krepselis.push(new Preke("Dviratis", 280, 1));
+krepselis.push(new Preke("Kede", 25, 2));
+krepselis.push(new PrekeKnyga("Knyga: Dievu Miskas", 20, 1));
+krepselis.push(new PrekeVaistai("Aspirinas", 5, 3));
+
+let sumaBePVM=0;
+let sumaSuPVM=0;
+krepselis.forEach((preke, i)=>{
+    console.log(i+1+". "+preke.pavadinimas+"\t"+preke.kaina+"EUR\t"+preke.kainaSuPVM().toFixed(2)+"EUR");
+    
+    sumaBePVM+=preke.kaina*preke.kiekis;
+    sumaSuPVM+=preke.kainaSuPVM()*preke.kiekis;
+});
+console.log("Be PVM ", sumaBePVM);
+console.log("Su PVM ", sumaSuPVM);
+
+
