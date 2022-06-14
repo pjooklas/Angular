a477989553150d919bc0f96d98e29fdb1b036702
pjooklas <pjooklas@users.noreply.github.com>
2022-06-14 20:57:10 +0300

    vaiku stovykla froma update

--- VaikuStovykla/src/app/directives/gimimo-validation.directive.ts
+++ VaikuStovykla/src/app/directives/gimimo-validation.directive.ts
@@ -1,10 +1,28 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors } from '@angular/forms';
 
 @Directive({
-  selector: '[appGimimoValidation]'
+  selector: '[gimimoValidation]',
+  providers:[
+    {
+      provide:NG_VALIDATORS,
+      useExisting:GimimoValidationDirective,
+      multi:true
+    }
+  ]
 })
 export class GimimoValidationDirective {
 
   constructor() { }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    let value:number=control.value;
+    if (value>=2010 && value<=2022){
+      return null;
+    }else{
+      return {error:"klaida"};
+    }
+   
+  }
 
 }
